d876ba097a881e5248057223364b111482635000
Antoniojr144 <antoniofranciscojr144@hotmail.com>
2020-02-14 11:48:02 -0200

    Exercicios 1.1 e 1.2

---
+++ Chapter1/Exercicio1.2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+using std::cout;
+using std::cin;
+using std::endl;
+
+  int main ()
+
+  {
+    int num1, num2, prod;
+
+      cout << "Digite o primeiro número\n";
+      cin >> num1;
+      
+      cout <<"Digite o segundo número\n";
+      cin >> num2;
+  
+      prod = (num1)*(num2);
+
+      cout << " O produto é " << prod << endl;
+
+      return 0;
+}	
