aa3006ddc10b8f16a134b724df29f32ae445bd1e
Pietro Chimenti <chimenti@localhost.localdomain>
2020-02-17 08:31:07 -0300

    Adicionamos cabeçalho e ajustamos indentação do código.

--- Chapter1/Exercicio1.1.cpp
+++ Chapter1/Exercicio1.1.cpp
@@ -1,10 +1,27 @@
+/**
+ * \ingroup Exercicios/Chapter1
+ *
+ * \brief Exercicio 1.1
+ *
+ * Solução do exercicio 1 do capitulo 1 do livro "Guide 
+ * to Scientific Computing in C++", de J. Pitt-Francios e J. Whiteley, 
+ * segunda edição; 
+ *
+ * \author Antonio Francisco Jr. (UEL)
+ *
+ * \version 1.0
+ *
+ * \date 17/02/2020
+ *
+ * contact: antoniofranciscojr144@hotmail.com
+ */
+
 #include<iostream>
 using std::cout;
 
-  int main ( int argc, char* argv[])
+int main( int argc, char* argv[] ){
 
-  {
-	cout << " Hello World\n";
+  cout << " Hello World!\n";
 
   return 0;
 }
--- Chapter1/Exercicio1.2.cpp
+++ Chapter1/Exercicio1.2.cpp
@@ -1,23 +1,39 @@
+/**
+ * \ingroup Exercicios/Chapter1
+ *
+ * \brief Exercicio 1.2
+ *
+ * Solução do exercicio 2 do capitulo 1 do livro "Guide 
+ * to Scientific Computing in C++", de J. Pitt-Francios e J. Whiteley, 
+ * segunda edição; 
+ *
+ * \author Antonio Francisco Jr. (UEL)
+ *
+ * \version 1.0
+ *
+ * \date 17/02/2020
+ *
+ * contact: antoniofranciscojr144@hotmail.com
+ */
+
 #include<iostream>
 #include<cmath>
 using std::cout;
 using std::cin;
 using std::endl;
 
-  int main ()
+int main(){
 
-  {
-    int num1, num2, prod;
+  int num1, num2, prod;
 
-      cout << "Digite o primeiro número\n";
-      cin >> num1;
+  cout << "Digite o primeiro número\n";
+  cin >> num1;
       
-      cout <<"Digite o segundo número\n";
-      cin >> num2;
+  cout <<"Digite o segundo número\n";
+  cin >> num2;
   
-      prod = (num1)*(num2);
+  prod = (num1)*(num2);
+  cout << "O produto é " << prod << "." << endl;
 
-      cout << " O produto é " << prod << endl;
-
-      return 0;
+  return 0;
 }	
