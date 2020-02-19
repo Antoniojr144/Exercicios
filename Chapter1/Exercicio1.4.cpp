4933ff4b91538f2bd9ec06ba67537c531f96f96c
Pietro Chimenti <chimenti@localhost.localdomain>
2020-02-19 10:35:50 -0300

    Correção ex. 4

--- Chapter1/Exercicio1.4.cpp
+++ Chapter1/Exercicio1.4.cpp
@@ -24,27 +24,46 @@ using std::endl;
 
 int main(){
 
-  double i,j,A,B,C,D;// Matrizes com ponto flutuante e i j para soma e multiplicação de matrizes.
-  
-  double array1[2][2] = { {5, 3}, {2,3}};
-  double array2[2][2] = { {7.3,56},{3.9,6}};
+  // possibilidade 1: declaração e inicialização juntos
+  double A[2][2] = {{5, 3}, {2,3}};
+  // possibilidade 2: primeiro declaração e depois assinação
+  double B[2][2], C[2][2], D[2][2],E[2][2];
+  B[0][0] = 7.3;
+  B[0][1] = 56;
+  B[1][0] = 3.9;
+  B[1][1] = 6;
 
-  cout << "Primeira Matriz A:" << array1[2][2]<<" "
-       << array1[2][2] << " "
-       << array1[2][2] << endl;
-  cout << "Segunda Matriz B:" << array2[2][2]<<" "
-       << array2[2][2] << " "
-       << array2[2][2] << endl;
+  cout << "Primeira Matriz A:\n" << A[0][0]<<" "
+       << A[0][1] << "\n"
+       << A[1][0] << " "
+       << A[1][1] << endl;
+  cout << "Segunda Matriz B:\n" << B[0][0]<<" "
+       << B[0][1] << "\n"
+       << B[1][0] << " "
+       << B[1][1] << endl;
 
-  for ( i = array1[0]; i < array1[4]; i= j+1)
-    for ( j = array1[0]; i < array2[4]; j = j+1)
+  for ( int i = 0; i < 2; ++i )
+    for ( int j = 0; j < 2; ++j ){
+      C[i][j]=A[i][j]+B[i][j];
+      D[i][j]=A[i][j]*B[i][j];// multiplicação elemento por elemento
+      E[i][j]=0;
+      for( int k = 0; k < 2; ++k )
+        E[i][j]+=A[i][k]*B[k][j];
+    }
 
-  C = array1[0]+ array2[0] 
+  cout << "Matriz soma: C=A+B:\n" << C[0][0]<<" "
+       << C[0][1] << "\n"
+       << C[1][0] << " "
+       << C[1][1] << endl;
+  cout << "Matriz produto elemento por elemento: D=AXB\n" << D[0][0]<<" "
+       << D[0][1] << "\n"
+       << D[1][0] << " "
+       << D[1][1] << endl;
+  cout << "Matriz produto linha por coluna: E=A*B\n" << E[0][0]<<" "
+       << E[0][1] << "\n"
+       << E[1][0] << " "
+       << E[1][1] << endl;
 
-  D = array1[0]*array2[0] 
-
-  cout << "A Matriz C é:" << C << endl;
-  cout << "A Matriz D é:" << D << endl;
 
   return 0;
 }
